7e0647bfa1db84b3bcf745fa4ac0f53444c913d8
batman-begins <abhinav.mg.k@gmail.com>
2023-01-17 18:23:46 +0530

    updates

--- finance_complaint/component/training/data_ingestion.py
+++ finance_complaint/component/training/data_ingestion.py
@@ -11,7 +11,7 @@ import pandas as pd
 import requests
 
 from finance_complaint.config.pipeline.training import FinanceConfig
-#from finance_complaint.config.spark_manager import spark_session
+from finance_complaint.config.spark_manager import spark_session
 from finance_complaint.entity.artifact_entity import DataIngestionArtifact
 from finance_complaint.entity.config_entity import DataIngestionConfig
 from finance_complaint.entity.metadata_entity import DataIngestionMetadata
